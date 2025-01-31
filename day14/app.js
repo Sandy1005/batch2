7a647ae954f43718ee1a8558c73077beb23482b1
Sandy1005 <polavarapusandy777@gamil.com>
2025-01-31 14:06:27 +0530

    mahesh

---
+++ day14/app.js
@@ -0,0 +1,15 @@
+// import x from"./child.js"
+// console.log(x);
+
+// !named import
+import college ,{x , user , users , obj} from "./child.js";
+console.log(college);
+console.log(x);
+console.log(user);
+
+console.log(users);
+users.map(user=>{
+    console.log(user);
+})
+
+console.log(obj.company);
---
+++ day14/child.js
@@ -0,0 +1,25 @@
+// let x=10;
+// export default x;
+
+let userDetails={
+    name:"Mahesh",
+    profession:"Manager"
+}
+export default userDetails;
+
+//named export
+let x = 10;
+const user = "Mahesh";
+const users = ["Mahesh","Varun","Rahul","Sandy"];
+const obj = {
+    name:"Mahesh",
+    company:"Microsoft"
+}
+
+//named export
+export{
+    x,
+    user,
+    users,
+    obj
+}
